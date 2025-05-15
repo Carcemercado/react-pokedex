00123d8a437472aef55eb695f19672bc81edc5e9
= <arce.code@gmail.com>
2025-05-15 12:26:33 -0400

    Basic search functionality added

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-# React + Vite
+# Pokedex Search with React + Vite + Axops + PokeAPI + Netlify
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
+Pokedex with search ability.
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
-## Expanding the ESLint configuration
+- [@axios](https://www.axios.com/) for fetching data.
 
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+- [@netlify](https://www.netlify.com/) for hosting.
+
+- [@pokeapi](https://pokeapi.co/) for RESTful Pokemon API.
+
 # react-pokedex
--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import axios from 'axios'
 import './App.css'
+import { useState } from 'react'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemon, setPokemon] = useState('Pikachu')
+  const [pokemonData, setPokemonData] = useState([])
+  const [pokemonType, setPokemonType] = useState([])
+  
+  const getPokemon = async () => {
+    const toArray = [];
+    try {
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+      const response = await axios.get(url)
+      toArray.push(response.data)
+      setPokemonType(response.data.types[0].type.name)
+      setPokemonData(toArray)
+      console.log(response)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  const handleChange = (e) => {
+    setPokemon(e.target.value.toLowerCase())
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    getPokemon()
+  }
+
+  let pokeName = pokemon.charAt(0).toUpperCase() + pokemon.slice(1)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Pokedex</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="pokemon">Enter Pokemon Name: </label>
+          <input type="text" id="pokemon" placeholder='Pokemon' onChange={handleChange}  />
+
+        </form>
+
+        {pokemonData.map((data) => {
+            return (
+            <div className="card" key={data.id}>
+              <h2>{pokeName}</h2>
+              <img src={data.sprites.front_default} alt={data.name} />
+              <ul>
+                <li>Type: {pokemonType}</li>
+                <li>Height: {data.height}</li>
+                <li>Weight: {data.weight}</li>
+                <li>Base Experience: {data.base_experience}</li>
+                <li>Moves:</li>
+              </ul>
+            </div>
+            )
+        })}
+
+        
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
